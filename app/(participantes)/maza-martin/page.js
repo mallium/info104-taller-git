6beaf100cb07727f0d19ad87da491d56c2f76e5f
martin-maza <martinmaza161@gmail.com>
2023-09-13 12:33:29 -0300

    Agregado maza-martin

--- app/(participantes)/maza-martin/page.js
+++ app/(participantes)/maza-martin/page.js
@@ -3,7 +3,7 @@ import style from "./style.module.css"
 import { Text, Container } from "@chakra-ui/react";
 
 export default function Ejemplo1() {
-  const nombre = "Pochinger";
+  const nombre = "PoShiTa";
   return (
     <main className={style.main}>
     <div className = {"contenedor " + style.fondo}>
