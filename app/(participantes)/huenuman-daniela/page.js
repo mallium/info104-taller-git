92fbaa3bed9f41bb9fb5e0e87f3bead03c88f577
Panqueque <108034714+Panquequ3@users.noreply.github.com>
2023-09-13 12:33:19 -0300

    cambios color

--- app/(participantes)/huenuman-daniela/page.js
+++ app/(participantes)/huenuman-daniela/page.js
@@ -5,7 +5,7 @@ import { Text, Container } from "@chakra-ui/react";
 export default function Ejemplo1() {
   const nombre = "Daniela Huenuman";
   return (
-    <div className="contenedor">
+    <div className={"contenedor" + style.nombre}>
       <Container>
         <center>
           <Text fontSize="xl" color="#ff57b4;" className="estiloNombre">
