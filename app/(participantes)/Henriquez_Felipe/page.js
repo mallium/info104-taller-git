59015640bf6037306eafe27a89a578ec05f6c15e
spartanpotato <107315314+spartanpotato@users.noreply.github.com>
2023-09-13 12:55:02 -0300

    cambie nombre de mi carpeta

--- app/(participantes)/Henriquez_Felipe/page.js
+++ app/(participantes)/Henriquez_Felipe/page.js
@@ -1,8 +1,6 @@
 "use client"
 
-import { background } from "@chakra-ui/react";
 import style from "./style.module.css";
-import { color } from "framer-motion";
 
 export default function Home() {
     return (
