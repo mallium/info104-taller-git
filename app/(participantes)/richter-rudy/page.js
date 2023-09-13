665f7f1bb1accd42c0ff1f7609195d627e7169a5
RudyRM <rudyrichterm@gmail.com>
2023-09-13 12:55:20 -0300

    agregando participante richter-rudy

--- app/(participantes)/richter-rudy/page.js
+++ app/(participantes)/richter-rudy/page.js
@@ -1,18 +1,17 @@
 "use client";
 
 import { Text, Container } from "@chakra-ui/react";
+import style from "./style.module.css";
+
 export default function Ejemplo1() {
   const nombre = "Rudy Richter";
   return (
-    <div className="contenedor">
+    <div className={"contenedor " + style.nombre2}>
       <Container>
         <center>
           <Text fontSize="xl" color="blue.500" className="estiloNombre">
             {nombre}
           </Text>
-          <text>
-            {Hola}
-          </text>
         </center>
       </Container>
     </div>
